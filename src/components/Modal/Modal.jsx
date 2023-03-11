436eeb64f0e33c5bf622325f73f110332d2fc48f
Artem Riabchych <artemskyart@gmail.com>
2023-03-11 15:26:51 +0200

    half-done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,37 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyDown);
-  }
+export const Modal = ({ children, closeModal }) => {
 
-  keyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.closeModal();
+
+  useEffect(() => {
+    const keyDown = evt => {
+      if (evt.code === 'Escape') {
+        closeModal();
+      }
+    };
+
+    // console.log("1");
+    window.addEventListener('keydown', keyDown);
+    return () => {
+      // console.log('2');
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [closeModal]);
+
+  const handleClose = evt => {
+    if (evt.currentTarget === evt.target) {
+      closeModal();
     }
   };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.keyDown);
-  }
-
-  handleClose = (evt) => {
-    if (evt.currentTarget === evt.target) {
-      this.props.closeModal();
-    }
-  }
-    
-  render() {
-    return createPortal(<div onClick={this.handleClose} className={css.Overlay}>
-      <div className={css.Modal}>{this.props.children}</div>
-    </div>, modalRoot)
-  }
-}
+  return createPortal(
+    <div onClick={handleClose} className={css.Overlay}>
+      <div className={css.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,52 @@
-import { Component } from "react";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { BiSearch } from 'react-icons/bi';
 import css from './Searchbar.module.css'
 
-export class Searchbar extends Component {
-  state = {
-    search: '',
+export const Searchbar = ({ handleSubmit }) => {
+  // state = {
+  //   search: '',
+  // };
+  const [search, setSearch] = useState('');
+
+  const onChangeInput = evt => {
+    setSearch(evt.currentTarget.value);
   };
 
-    onChangeInput = (evt) => {
-        const { name, value } = evt.currentTarget;
-        this.setState({ [name]: value });
-  }
+  const resetForm = () => {
+    setSearch('');
+  };
 
-    resetForm = () => {
-     this.setState({ search: '' });
-    }
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form
-          onSubmit={evt => {
-                    evt.preventDefault();
-                    if (!this.state.search) {
-                      return toast.error('Enter text for search.');
-                    }
-            this.props.handleSubmit(this.state.search);
-            this.resetForm();
-          }}
-          className={css.Form}
-        >
-          <button type="submit" className={css.Button}>
-            {/* <span className={css.buttonLabel}> */}
-            <BiSearch size="20" />
-            {/* </span> */}
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form
+        onSubmit={evt => {
+          evt.preventDefault();
+          if (!search) {
+            return toast.error('Enter text for search.');
+          }
+          handleSubmit(search);
+          resetForm();
+        }}
+        className={css.Form}
+      >
+        <button type="submit" className={css.Button}>
+          {/* <span className={css.buttonLabel}> */}
+          <BiSearch size="20" />
+          {/* </span> */}
+        </button>
 
-          <input
-            value={this.state.search}
-            onChange={this.onChangeInput}
-            className={css.Input}
-            name="search"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          value={search}
+          onChange={onChangeInput}
+          className={css.Input}
+          name="search"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
